d8590860748e95288038a134a10e74d0e2f0586d
Tyrell H <73722828+TyrellHaywood@users.noreply.github.com>
2024-09-29 01:22:25 -0400

    feat: signup?

--- frontend/matchbox/app/login/page.tsx
+++ frontend/matchbox/app/login/page.tsx
@@ -6,27 +6,29 @@ export default function Login() {
       <h1 className="text-4xl mt-[50px]">Matchbox</h1>
       <div className="w-[80vw] fixed top-[45%]">
         <form className="w-full flex justify-centers items-center flex-col gap-5">
-          <div className=" border-2 border-black rounded-md w-full h-10">
-            <label htmlFor="email">Email:</label>
+          <div className=" border-2 border-black rounded-md w-full h-10 flex justify-between items-center">
             <input
               className="outline-none ml-2"
               type="email"
               id="email"
               name="email"
+              placeholder="my@example.com"
               required
             />
+            <p className="text-red-500 mr-4">*</p>
           </div>
-          <div className=" border-2 border-black rounded-md w-full h-10">
-            <label htmlFor="password">Password:</label>
+          <div className=" border-2 border-black rounded-md w-full h-10 flex justify-between items-center ">
             <input
               className="outline-none ml-2"
               type="password"
               id="password"
               name="password"
+              placeholder="Password"
               required
             />
+            <p className="text-red-500 mr-4">*</p>
           </div>
-          <button type="submit" className="w-1/2 rounded-md h-10 bg-gray-300">
+          <button type="submit" className="w-1/2 rounded-md h-10  bg-gray-300">
             Login
           </button>
         </form>
