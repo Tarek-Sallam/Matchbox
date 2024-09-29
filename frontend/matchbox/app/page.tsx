08be2992cd080e896749acf4563e24e45078e3c9
swesan123 <201360on50@gmail.com>
2024-09-29 01:55:36 -0400

    Made a filtermenu compononent

--- frontend/matchbox/app/page.tsx
+++ frontend/matchbox/app/page.tsx
@@ -1,5 +1,6 @@
 import SwipeCard from "@/components/SwipeCard";
 import BottomTab from "@/components/BottomTab";
+import FilterMenu from "@/components/FilterMenu";
 
 export default function Homepage() {
   return (
