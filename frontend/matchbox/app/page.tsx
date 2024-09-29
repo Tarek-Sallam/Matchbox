49fc651fed8a25b5d2b40d04f8e5066fed14211f
swesan123 <201360on50@gmail.com>
2024-09-29 01:55:36 -0400

    Made a filtermenu compononent

--- frontend/matchbox/app/page.tsx
+++ frontend/matchbox/app/page.tsx
@@ -3,22 +3,12 @@
 import SwipeCard from "@/components/SwipeCard";
 import BottomTab from "@/components/BottomTab";
 import FilterMenu from "@/components/FilterMenu";
-import { useState } from "react";
 
 export default function Homepage() {
-  const [isFilterVisible, setIsFilterVisible] = useState(false);
-
-  function toggleFilter(event: React.MouseEvent<HTMLButtonElement>): void {
-    setIsFilterVisible(!isFilterVisible);
-  }
-
-  function closeFilter(): void {
-    setIsFilterVisible(false);
-  }
-
   return (
-    <div className="flex flex-col h-[100vh] justify-end">
-      <div className="flex justify-center align-center">
+    <div className="relative flex flex-col h-screen">
+      <FilterMenu />
+      <div className="flex-grow flex justify-center items-center">
         <SwipeCard />
       </div>
       <div className="flex justify-center align-center">
