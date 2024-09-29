2fafa1b3c5687b9cc22c431e962b4cbd00912fd2
Tyrell H <73722828+TyrellHaywood@users.noreply.github.com>
2024-09-29 11:00:46 -0400

    fixifiix

--- frontend/matchbox/components/Skill.tsx
+++ frontend/matchbox/components/Skill.tsx
@@ -5,7 +5,7 @@ interface SkillProps {
 export default function SwipeCard({ skillsOff }: SkillProps) {
   return (
     <div className="flex justify-center items-center text-center w-auto h-[25px] rounded-md bg-gray-200">
-      <p>{skillsOff}</p>
+      <p>skill</p>
     </div>
   );
 }
